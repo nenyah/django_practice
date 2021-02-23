d95e22c4a70238242e99cf269fd9ff918b85c52f
steven <steven@nenyah.com>
2021-02-23 13:29:06 +0800

    feat(*): Learn drf serializer and modelseriralizer

--- book/urls.py
+++ book/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from users.views import BookAPIView1
+from users.views import BookAPIView1, BookAPIView2
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('apibook1/', BookAPIView1.as_view(), name='book1')
+    path('apibook1/', BookAPIView1.as_view(), name='book1'),
+    path('apibook2/', BookAPIView2.as_view(), name='book2'),
 ]
--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import UserProfile, Book
+
+from .models import Book
 
 
 class BookSerializer(serializers.Serializer):
@@ -8,3 +9,10 @@ class BookSerializer(serializers.Serializer):
     author = serializers.CharField(required=True, max_length=100)
     publish = serializers.CharField(required=True, max_length=100)
     rate = serializers.FloatField(default=0)
+
+
+class BookModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        # fields = "__all__"  # 将整个表的所有字段都序列化
+        fields = ('title', 'isbn', 'author')
